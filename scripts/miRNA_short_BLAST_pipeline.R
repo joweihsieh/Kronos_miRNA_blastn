8d719caa986944f0b45dbeed9ba03f33b099016b
Jo-Wei Allison Hsieh <113166697+joweihsieh@users.noreply.github.com>
2025-08-07 10:52:14 -0700

    Update miRNA_short_BLAST_pipeline.R

--- scripts/miRNA_short_BLAST_pipeline.R
+++ scripts/miRNA_short_BLAST_pipeline.R
@@ -6,7 +6,7 @@ library(dplyr)
 library(readr)
 library(stringr)
 
-# ========== 參數輸入 ==========
+# ========== Parameters ==========
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) != 6) {
   stop("Usage: script.R <xlsx_path> <sheet_name> <base_prefix> <genome_db> <genome_fa> <premiRNA_bed>")
@@ -43,20 +43,20 @@ merged_txt <- str_replace(final_bed, "\\.bed$", "_merged.txt")
 summary_txt <- str_replace(final_bed, "\\.bed$", "_summary.tsv")
 details_txt <- str_replace(final_bed, "\\.bed$", "_details.tsv")
 
-# ========== 1. 建立 FASTA ==========
+# ========== 1. FASTA ==========
 df <- read_excel(xlsx_path, sheet = sheet_name, skip = 1)
 df2 <- df[grep("Ttu", df$Chromosome), ]
 fasta <- DNAStringSet(df2$miRNA)
 names(fasta) <- df2$Name
 writeXStringSet(fasta, filepath = output_fasta)
 
-# ========== 2. 執行 BLAST ==========
+# ========== 2. Run BLAST ==========
 cmd <- sprintf("blastn -query %s -strand both -task blastn-short -db %s -out %s -word_size 15 -perc_identity 100 -no_greedy -ungapped -dust no -outfmt 6",
                output_fasta, genome_db, blast_result)
 message(cmd)
 system(cmd)
 
-# ========== 3. 過濾比對 ==========
+# ========== 3. Filter ==========
 query_seqs <- readDNAStringSet(output_fasta)
 df <- read_tsv(blast_result, col_names = FALSE, show_col_types = FALSE)
 colnames(df)[1:12] <- c("qseqid","sseqid","pident","length","mismatch","gapopen",
@@ -77,14 +77,14 @@ bed_df <- df_filtered %>%
   ) %>% select(chrom, start, end, name, total_mismatch, strand)
 write_tsv(bed_df, filtered_bed, col_names = FALSE)
 
-# ========== 4. 交集 precursor ==========
+# ========== 4. precursor ==========
 system(sprintf("bedtools intersect -a %s -b %s -wa -wb > %s", filtered_bed, precursor_bed, filtered_clean))
 system(sprintf("awk '$4 == $10{print $1, $2, $3, $4, $5, $6}' OFS=\"\t\" %s > %s", filtered_clean, final_bed))
 file.remove(filtered_bed, filtered_clean, filtered_tsv, blast_result)
 
-# ========== 5. 取 genome 序列並轉為大寫 ==========
+# ========== 5. Extract sequence from genome ==========
 system(sprintf("bedtools getfasta -fi %s -bed %s -fo %s -s", genome_fa, final_bed, output_seq))
 system(sprintf("awk '{if ($0 ~ /^>/) print $0; else print toupper($0)}' %s > %s", output_seq, output_seq_up))
 
-# ========== 6. 合併 BED 與序列 ==========
+# ========== 6. Combine bed and sequence ==========
 system(sprintf("awk 'FNR==NR { if ($0 ~ /^>/) { gsub(/^>/, \"\", $0); key = $0; getline; seqs[key] = $0; next } } { key = $1\":\"$2\"-\"$3\"(\"$6\")\"; seq = (key in seqs) ? seqs[key] : \"NA\"; print $1, $2+1, $3, $4, $5, $6, seq }' %s %s OFS=\"\t\" > %s", output_seq_up, final_bed, merged_txt))
